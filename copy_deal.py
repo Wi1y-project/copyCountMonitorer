eb4f48863cf95f8ce625deda4a806c80d6f67e3b
macbook pro <spurs@ubuntu.com>
2025-06-07 22:57:15 +0800

    获取带单人数据插入数据库

--- copy_deal.py
+++ copy_deal.py
@@ -1,7 +1,10 @@
 import hashlib
+import json
 import time
-
+import logging
 import pymongo
+import requests
+
 from api_requests import get_deal
 connect = pymongo.MongoClient(
             'mongodb://root:Love3597320@127.0.0.1:27017/?authSource=admin',
@@ -9,6 +12,34 @@ connect = pymongo.MongoClient(
             waitQueueTimeoutMS=1000  # 设置等待队列的超时时间
         )
 
+
+def dd_sender(text):
+    """
+    发送钉钉通知
+
+    Args:
+        text (str): 要发送的通知消息内容
+    """
+    headers = {
+        "Content-Type": "application/json"
+    }
+    url = "https://oapi.dingtalk.com/robot/send"
+    params = {
+        "access_token": "10eaa0ee407dc6ddb4c87ba0e99ef9943e7cbb53912f6ef4004d1751978266b3"
+    }
+    data = {
+        "msgtype": "text",
+        "text": {
+            "content": f"通知： 线上Gemini出错:{text}"
+        }
+    }
+    try:
+        data = json.dumps(data, separators=(',', ':'))
+        response = requests.post(url, headers=headers, params=params, data=data)
+        response.raise_for_status()
+        logging.info("钉钉通知发送成功")
+    except requests.exceptions.RequestException as e:
+        logging.error(f"钉钉通知发送失败: {e}")
 
 class CopyDeal:
     def __init__(self):
@@ -45,9 +76,15 @@ class CopyDeal:
                 each_data['_id'] = md5_str
                 self.db.insert_one(each_data)
 
+                dd_data = {
+                    '交易对': each_data.get('symbol', 'N/A'),
+                    '交易方向': each_data.get('side', 'N/A'),
+                    '成交价格': each_data.get('avgPrice', 'N/A'),
+                    '成交数量': each_data.get('executedQty', 'N/A'),
+                }
+
+                dd_sender(f"{dd_data}")
             time.sleep(30)
-
-
 
 
 if __name__ == "__main__":
